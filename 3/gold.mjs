23ab981c0a90a40ed3bfec99809cc2efb5cc3bf7
adamjc <adamjc86@gmail.com>
2022-12-13 22:21:08 +0000

    Add Day 3 gold solution

--- 3/gold.mjs
+++ 3/gold.mjs
@@ -1,8 +1,28 @@
-import { pipe, tap } from "ramda";
+import { add, equals, filter, find, isNil, map, mergeAll, not, pipe, reduce, split, splitEvery, take, tap, unnest } from "ramda";
 import { readInput } from "../helpers.mjs";
+
+const alphaMap = pipe(
+  () => "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  split(''),
+  alphabet => alphabet.map((el, i) => ({[el]: i + 1})),
+  mergeAll
+)()
+
+// find as in bs
+const findit = ([a, b, c]) => pipe(
+  ([a, _]) => split("", a),
+  map(a => find(equals(a), b) && find(equals(a), c)),
+  filter(pipe(isNil, not)),
+  take(1)
+)([a, b, c])
 
 const main = pipe(
   readInput,
+  splitEvery(3), // split into groups of 3
+  map(findit), // find the 1 common character across all 3 strings
+  unnest,
+  map(el => alphaMap[el]), // convert to priorities (a-z = 1-26, A-Z = 27-52),
+  reduce(add, 0),
   tap(console.log)
 )
 
