e1693dd9151a6789afe93d34d5f436c62dbcc580
adamjc <adamjc86@gmail.com>
2022-12-14 21:24:34 +0000

    Add day 1 solution (broken)

--- 4/main.mjs
+++ 4/main.mjs
@@ -1,10 +1,35 @@
-import { pipe, tap } from "ramda";
+import { equals, filter, map, pipe, split, tap, length } from "ramda";
 import { readInput } from "../helpers.mjs";
 
 // In how many assignment pairs does one range fully contain the other?
 
+// checks if b is bounded by a, or if a is bounded by b
+const isBounded = ([[aLow, bLow], [aUp, bUp]]) => 
+  (aLow <= bLow && aUp >= bUp) ||
+  (bLow <= aLow && bUp >= aUp)
+
+// puts lower and upper bounds together
+const parseBounds = pipe(
+  split(','),
+  ([a, b]) => {
+    return [[
+      a.split('-')[0],
+      b.split('-')[0]
+    ], [
+      a.split('-')[1],
+      b.split('-')[1]
+    ]]
+  }
+)
+
 const main = pipe(
   readInput,
+  tap(console.log),
+  map(pipe(parseBounds, isBounded)),
+  tap(console.log),
+  filter(equals(true)),
+  tap(console.log),
+  length,
   tap(console.log)
 )
 
