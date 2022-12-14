829c2b917cd5c38fd5aa8b53679db6909e02734d
adamjc <adamjc86@gmail.com>
2022-12-14 21:58:33 +0000

    Add Day 4 solution

--- 4/main.mjs
+++ 4/main.mjs
@@ -4,20 +4,18 @@ import { readInput } from "../helpers.mjs";
 // In how many assignment pairs does one range fully contain the other?
 
 // checks if b is bounded by a, or if a is bounded by b
-const isBounded = ([[aLow, bLow], [aUp, bUp]]) => 
-  (aLow >= bLow && aUp <= bUp) ||
-  (bLow >= aLow && bUp <= aUp)
+const isBounded = ([[aLow, bLow], [aUp, bUp]]) => (aLow >= bLow && aUp <= bUp) || (bLow >= aLow && bUp <= aUp)
 
 // puts lower and upper bounds together
 const parseBounds = pipe(
   split(','),
   ([a, b]) => {
     return [[
-      a.split('-')[0],
-      b.split('-')[0]
+      Number(a.split('-')[0]),
+      Number(b.split('-')[0])
     ], [
-      a.split('-')[1],
-      b.split('-')[1]
+      Number(a.split('-')[1]),
+      Number(b.split('-')[1])
     ]]
   }
 )
@@ -26,6 +24,7 @@ const main = pipe(
   readInput,
   tap(console.log),
   map(parseBounds),
+  tap(console.log),
   map(isBounded),
   tap(console.log),
   filter(equals(true)),
