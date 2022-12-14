212963c389e06897c7914459521f39709afb32ca
adamjc <adamjc86@gmail.com>
2022-12-14 22:41:35 +0000

    Add Day 4 part 2

--- 4/gold.mjs
+++ 4/gold.mjs
@@ -1,8 +1,28 @@
-import { pipe, tap } from "ramda";
+import { equals, filter, map, pipe, split, tap, length, tail, dropLast } from "ramda";
 import { readInput } from "../helpers.mjs";
+
+// In how many pairs does one range fully contain the other?
+
+// checks if any b is in a, or if any a is in b
+const overlaps = ([[aLow, bLow], [aUp, bUp]]) => aLow <= bUp && aUp >= bLow;
+
+// puts lower and upper bounds together
+const parseBounds = pipe(
+  split(','),
+  ([a, b]) => [[
+    Number(a.split('-')[0]),
+    Number(b.split('-')[0])
+  ], [
+    Number(a.split('-')[1]),
+    Number(b.split('-')[1])
+  ]]
+)
 
 const main = pipe(
   readInput,
+  map(pipe(parseBounds, overlaps)),
+  filter(equals(true)),
+  length,
   tap(console.log)
 )
 
--- 4/main.mjs
+++ 4/main.mjs
@@ -1,32 +1,28 @@
 import { equals, filter, map, pipe, split, tap, length, tail, dropLast } from "ramda";
 import { readInput } from "../helpers.mjs";
 
-// In how many assignment pairs does one range fully contain the other?
+// In how many pairs does one range fully contain the other?
 
 // checks if b is bounded by a, or if a is bounded by b
-const isBounded = ([[aLow, bLow], [aUp, bUp]]) => (aLow >= bLow && aUp <= bUp) || (bLow >= aLow && bUp <= aUp)
+const isBounded = ([[aLow, bLow], [aUp, bUp]]) => 
+  (aLow >= bLow && aUp <= bUp) ||
+  (bLow >= aLow && bUp <= aUp)
 
 // puts lower and upper bounds together
 const parseBounds = pipe(
   split(','),
-  ([a, b]) => {
-    return [[
-      Number(a.split('-')[0]),
-      Number(b.split('-')[0])
-    ], [
-      Number(a.split('-')[1]),
-      Number(b.split('-')[1])
-    ]]
-  }
+  ([a, b]) => [[
+    Number(a.split('-')[0]),
+    Number(b.split('-')[0])
+  ], [
+    Number(a.split('-')[1]),
+    Number(b.split('-')[1])
+  ]]
 )
 
 const main = pipe(
   readInput,
-  tap(console.log),
-  map(parseBounds),
-  tap(console.log),
-  map(isBounded),
-  tap(console.log),
+  map(pipe(parseBounds, isBounded)),
   filter(equals(true)),
   length,
   tap(console.log)
