caad3089104e9984034ef4e2da6de4764b669d96
adamjc <adamjc86@gmail.com>
2022-12-17 12:22:38 +0000

    Add initial day 5
    - Improve newday script

--- newday.sh
+++ newday.sh
@@ -1,7 +1,7 @@
 day=$1
 
 mkdir $1
-cp boilerplate.mjs $1/main.mjs
-cp boilerplate.mjs $1/gold.mjs
+cp boilerplate.mjs $1/part-1.mjs
+cp boilerplate.mjs $1/part-2.mjs
 touch $1/instructions
 touch $1/input
