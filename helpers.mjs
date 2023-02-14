282fcc934550c637c0b8ab9d6c9f8be512276161
adamjc <adamjc86@gmail.com>
2023-02-14 20:43:01 +0000

    Add helper for relative pathing
    - Now can run exercises regardless of pwd

--- helpers.mjs
+++ helpers.mjs
@@ -1,9 +1,15 @@
-import {readFileSync} from 'fs'
+import { readFileSync } from 'fs'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const path = (loc, file) => join(dirname(fileURLToPath(loc)), file)
 
 function readInput (filePath) {
+  console.log(filePath)
   return readFileSync(filePath, 'utf8').split("\n").slice(0, -1)
 }
 
 export {
-  readInput
+  readInput,
+  path
 }
